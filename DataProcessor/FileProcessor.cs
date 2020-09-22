26f1c587546f2a9a5881cd3820c31d5c933ec643
Ben Nesbit <benesbit@yahoo.com>
2020-09-22 16:13:18 -0700

    Display the root directory to the user.

--- DataProcessor/FileProcessor.cs
+++ DataProcessor/FileProcessor.cs
@@ -22,6 +22,9 @@ namespace DataProcessor
                 WriteLine($"ERROR: file {InputFilePath} does not exist.");
                 return;
             }
+
+            string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
+            WriteLine($"Root data path is {rootDirectoryPath}");
         }
     }
 }
