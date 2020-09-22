218b0095a4ec68d14dd506e71f8f6901128a02f7
Ben Nesbit <benesbit@yahoo.com>
2020-09-22 16:00:43 -0700

    Create FileProcessor class.

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace DataProcessor
+{
+    internal class FileProcessor
+    {
+        public string InputFilePath { get;  }
+
+        public FileProcessor(string filePath)
+        {
+            this.filePath = filePath;
+        }
+    }
+}
