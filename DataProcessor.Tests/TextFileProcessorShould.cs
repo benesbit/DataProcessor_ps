b884642179b5566ccf92cf29cf31e8afcbdc82f3
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 16:55:26 -0700

    Stub out test: write comments for steps we need to take.

--- DataProcessor.Tests/TextFileProcessorShould.cs
+++ DataProcessor.Tests/TextFileProcessorShould.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace DataProcessor.Tests
 {
     public class TextFileProcessorShould
     {
         [Fact]
-        public void Test1()
+        public void MakeSecondLineUpperCase()
         {
+            // Create a mock file input
 
+            // Setup mock file system
+
+            // Create TextFileProcessor with mock file system
+
+            // Process test file
+
+            // Check mock file system for output file
+
+            // Check content of output file in mock file system
         }
     }
 }
