efdf8ee46db9de2544e31e643efb20da0616607c
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 11:35:38 -0700

    Stub out new 'TextFileProcessor' class.

--- DataProcessor/TextFileProcessor.cs
+++ DataProcessor/TextFileProcessor.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace DataProcessor
 {
     class TextFileProcessor
     {
+        public string InputFilePath { get; }
+        public string OutputFilePath { get; }
+
+        public TextFileProcessor(string inputFilePath, string outputFilePath)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+        }
+
+        public void Process()
+        {
+
+        }
     }
 }
