186705842edc6dee7dbd8a5940f5bd107efaeef3
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 13:31:07 -0700

    Add logic

--- DataProcessor/CsvFileProcessor.cs
+++ DataProcessor/CsvFileProcessor.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,21 @@ namespace DataProcessor
 {
     class CsvFileProcessor
     {
+        public string InputFilePath { get; }
+        public string OutputFilPath { get; }
+
+        public CsvFileProcessor(string inputFilePath, string outputFilePath)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilPath = outputFilePath;
+        }
+        public void Process()
+        {
+            using (StreamReader input = File.OpenText(InputFilePath))
+            using (CsvReader csvReader = new CsvReader(input, System.Globalization.CultureInfo.CreateSpecificCulture("enUS")))
+            {
+
+            }
+        }
     }
 }
