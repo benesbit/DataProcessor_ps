756c10cd088ddd3797e88d21618e7a7eeb9567d6
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 11:51:19 -0700

    Use a string array to read lines in/out.
    Previously, the whole file was read into a single string object, now we are using an array of strings, with each string representing a line of text from the file.

--- DataProcessor/TextFileProcessor.cs
+++ DataProcessor/TextFileProcessor.cs
@@ -16,9 +16,14 @@ namespace DataProcessor
         public void Process()
         {
             // Using read all text
-            string originalText = File.ReadAllText(InputFilePath);
-            string proccesedText = originalText.ToUpperInvariant();
-            File.WriteAllText(OutputFilePath, proccesedText);
+            //string originalText = File.ReadAllText(InputFilePath);
+            //string proccesedText = originalText.ToUpperInvariant();
+            //File.WriteAllText(OutputFilePath, proccesedText);
+
+            // Using read all lines
+            string[] lines = File.ReadAllLines(InputFilePath);
+            lines[1] = lines[1].ToUpperInvariant(); // Assume at least 2 lines in the file
+            File.WriteAllLines(OutputFilePath, lines);
         }
     }
 }
