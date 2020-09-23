cf154a3299c076250eed2876e7741ae22202c030
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 15:40:51 -0700

    Create class using inheritance from ClassMap<ProcessedOrder>

---
+++ DataProcessor/ProcessedOrderMap.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace DataProcessor
+{
+    class ProcessedOrderMap : ClassMap<ProcessedOrder>
+    {
+        public ProcessedOrderMap()
+        {
+            AutoMap(CultureInfo.InvariantCulture);
+
+            Map(m => m.Customer).Name("CustomerNumber");
+            Map(m => m.Amount).Name("Quantity");
+        }
+    }
+}
