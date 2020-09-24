fdae846409d37fe6493fb3f2a8997430aa7321ce
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 17:43:21 -0700

    Make class public for testability.

--- DataProcessor/BinaryFileProcessor.cs
+++ DataProcessor/BinaryFileProcessor.cs
@@ -3,7 +3,7 @@ using System.IO.Abstractions;
 
 namespace DataProcessor
 {
-    class BinaryFileProcessor
+    public class BinaryFileProcessor
     {
         private readonly IFileSystem _fileSystem;
 
