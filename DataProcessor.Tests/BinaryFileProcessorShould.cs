7e93f4a10eb0b5bf0b7480d8601b8f3dbab68846
Ben Nesbit <benesbit@yahoo.com>
2020-09-23 17:35:17 -0700

    Begin stub out of test.

--- DataProcessor.Tests/BinaryFileProcessorShould.cs
+++ DataProcessor.Tests/BinaryFileProcessorShould.cs
@@ -1,10 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Xunit;
+using System.IO.Abstractions.TestingHelpers;
 
 namespace DataProcessor.Tests
 {
-    class BinaryFileProcessorShould
+    public class BinaryFileProcessorShould
     {
+        [Fact]
+        public void AddLargestNumber()
+        {
+            var mockInputFile = new MockFileData(new byte[] { 0xFF, 0x34, 0x56, 0xD2 });
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddFile(@"c:\root\in\myfile.data", mockInputFile);
+            mockFileSystem.AddDirectory(@"c:\root\out");
+
+            //var sut = new BinaryFileProcessor(@"c:\root\in\myfile.data",
+            //                                  @"c:\root\out\myfile.data",)
+        }
     }
 }
